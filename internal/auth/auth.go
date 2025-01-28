6326f798739874e356990efac56ca22216deb3a4
Roger Chen <rjchen83@gmail.com>
2025-01-27 20:48:47 -0800

    Added GetApiKey tests

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"net/http"
 	"strings"
+	"testing"
 )
 
 var ErrNoAuthHeaderIncluded = errors.New("no authorization header included")
@@ -20,4 +21,45 @@ func GetAPIKey(headers http.Header) (string, error) {
 	}
 
 	return splitAuth[1], nil
+}
+
+func TestGetAPIKey(t *testing.T) {
+	tests := []struct {
+		name    string
+		headers http.Header
+		want    string
+		wantErr error
+	}{
+		{
+			name:    "No Authorization header",
+			headers: http.Header{},
+			want:    "Asdfdsafdsf",
+			wantErr: ErrNoAuthHeaderIncluded,
+		},
+		{
+			name: "Malformed Authorization header",
+			headers: http.Header{
+				"Authorization": []string{"Bearer token"},
+			},
+			want:    "sdfsf",
+			wantErr: errors.New("malformed authorization header"),
+		},
+		{
+			name: "Correct Authorization header",
+			headers: http.Header{
+				"Authorization": []string{"ApiKey correct_api_key"},
+			},
+			want:    "correct_api_key",
+			wantErr: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := GetAPIKey(tt.headers)
+			if got != tt.want || (err != nil && err.Error() != tt.wantErr.Error()) {
+				t.Errorf("GetAPIKey() = %v, %v; want %v, %v", got, err, tt.want, tt.wantErr)
+			}
+		})
+	}
 }
